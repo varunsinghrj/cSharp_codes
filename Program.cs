de00ec12944371eac24026821d001cee1e06f138
VARUN SINGH <varunsinghrj4@gmail.com>
2024-07-28 21:32:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("size of bool : "+sizeof(bool)+ " bit");
+         System.Console.WriteLine("size of char : "+sizeof(char)+ " bytes");
+          System.Console.WriteLine("size of int : "+sizeof(int)+ " bytes");
+           System.Console.WriteLine("size of long : "+sizeof(long)+ " bytes");
+           System.Console.WriteLine("size of float : "+sizeof(float)+ " bytes");
+           System.Console.WriteLine("size of double : "+sizeof(double)+ " bytes");
+           int a= 5,b=10;
+           System.Console.WriteLine(a+b);
+    }
+}
